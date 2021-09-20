4598bea41c04bbade71a10701f1108036e7fe772
Matsu <huhta.matias@gmail.com>
2021-09-20 10:57:34 +0300

    Update nvm version

--- init-node.sh
+++ init-node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "===== Installing nvm ====="
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
 echo "===== Verifying nvm installation ===="
 exec bash
