a1550fb80fcfd14e8ff2783ff17df7b5a381f3ce
Matsuuu <huhta.matias@gmail.com>
2022-07-05 14:40:23 +0300

    Ported compe settings

--- init.lua
+++ init.lua
@@ -1,7 +1,13 @@
 
 
 require 'rcfiles.plugins'
+require 'rcfiles.plugins.compe'
+-- source ~/.vim/rcfiles/lua/lspconfig.vim
+-- source ~/.vim/rcfiles/lua/rest-nvim.vim
+-- source ~/.vim/rcfiles/lua/telescope.vim
+-- source ~/.vim/rcfiles/lua/treesitter.vim
+-- source ~/.vim/rcfiles/lua/nvim_dap.vim
 vim.cmd([[
 
-    source ~/.vimrc
+    " source ~/.vimrc
 ]])
