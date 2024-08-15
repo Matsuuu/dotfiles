1f2811e11402461052478a3dee5da5091324e4a6
Matsuuu <huhta.matias@gmail.com>
2024-08-15 20:37:27 +0300

    Fix those keymaps goddammit

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -35,7 +35,8 @@ return {
         "saadparwaiz1/cmp_luasnip",
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-cmdline",
-        "hrsh7th/cmp-buffer"
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-nvim-lsp-signature-help"
     },
     opts = function(_, opts)
         local cmp = require("cmp")
@@ -45,6 +46,7 @@ return {
                   { name = 'nvim_lsp' },
                   { name = 'luasnip' },
                   { name = 'buffer' },
+                  { name = "nvim_lsp_signature_help" },
           })
 
         opts.snippet = {
