86b5bedb76d17d8c25a8af10453b4a4ee23de888
Matsuuu <huhta.matias@gmail.com>
2024-08-26 22:36:24 +0300

    Add conform

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,17 @@
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function(args)
+    require("conform").format({ bufnr = args.buf })
+  end,
+})
+
+return {
+  'stevearc/conform.nvim',
+  opts = {
+        formatters_by_ft = {
+            -- Conform will run the first available formatter
+            javascript = { "prettierd", "prettier", stop_after_first = true },
+        }
+    },
+}
+
