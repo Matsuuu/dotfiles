a4164d118349fa86011a941c1a7651dce61a7afb
Matsuuu <huhta.matias@gmail.com>
2025-04-26 11:35:56 +0300

    Rip nvimtree

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,21 +1,34 @@
 return {
-    "stevearc/oil.nvim",
-    ---@module "oil"
-    ---@type oil.SetupOpts
-    opts = {},
-    dependencies = {
-        -- { "echanovski/mini.icons", opts = {} }
-    },
-    lazy = false,
-    config = function ()
-        require("oil").setup({
-            columns = { "icon" },
-            view_options = {
-                show_hidden = true,
-            },
-        })
+	"stevearc/oil.nvim",
+	---@module "oil"
+	---@type oil.SetupOpts
+	opts = {},
+	dependencies = {
+		-- { "echanovski/mini.icons", opts = {} }
+	},
+	lazy = false,
+	config = function()
+		require("oil").setup({
+			columns = { "icon" },
+			view_options = {
+				show_hidden = true,
+			},
+			-- Configuration for the file preview window
+			preview_win = {
+				-- Whether the preview window is automatically updated when the cursor is moved
+				update_on_cursor_moved = true,
+				-- How to open the preview window "load"|"scratch"|"fast_scratch"
+				preview_method = "fast_scratch",
+				-- A function that returns true to disable preview on a file e.g. to avoid lag
+				disable_preview = function(filename)
+					return false
+				end,
+				-- Window-local options to use for preview window buffers
+				win_options = {},
+			},
+		})
 
-        vim.keymap.set('n', '-', '<cmd>Oil<CR>')
-        vim.keymap.set('n', '<leader>-', require("oil").toggle_float)
-    end
+		vim.keymap.set("n", "-", "<cmd>Oil<CR>")
+		vim.keymap.set("n", "<leader>-", require("oil").toggle_float)
+	end,
 }
