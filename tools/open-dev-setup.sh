53f9704495375cd1c738eab15ac1c97328a7bcb1
Matsuuu <huhta.matias@gmail.com>
2025-06-26 08:35:37 +0300

    Update tools

---
+++ tools/open-dev-setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+i3-msg "workspace 1; exec google-chrome-stable"
+sleep 0.5
+i3-msg "workspace 2; exec alacritty"
+sleep 0.5
+i3-msg "workspace 3; exec google-chrome-stable"
+sleep 0.5
+i3-msg "workspace 4; exec google-chrome-stable"
+sleep 0.5
