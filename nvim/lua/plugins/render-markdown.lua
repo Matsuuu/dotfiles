c80e05573f56e2c2589bc40f073a77566a65bd7e
Matsuuu <huhta.matias@gmail.com>
2026-02-27 07:46:14 +0200

    Reintro jsconfig and enhance markdown

--- nvim/lua/plugins/render-markdown.lua
+++ nvim/lua/plugins/render-markdown.lua
@@ -7,5 +7,24 @@ return {
     ---@type render.md.UserConfig
     opts = {
         completions = { lsp = { enabled = true } },
+
+        checkbox = {
+        unchecked = {
+            -- Replaces '[ ]' of 'task_list_marker_unchecked'.
+            icon = '󰄱 ',
+            -- Highlight for the unchecked icon.
+            highlight = 'RenderMarkdownUnchecked',
+            -- Highlight for item associated with unchecked checkbox.
+            scope_highlight = nil,
+        },
+        checked = {
+            -- Replaces '[x]' of 'task_list_marker_checked'.
+            icon = '☑',
+            -- Highlight for the checked icon.
+            highlight = 'RenderMarkdownChecked',
+            -- Highlight for item associated with checked checkbox.
+            scope_highlight = nil,
+        },
+    },
     },
 }
